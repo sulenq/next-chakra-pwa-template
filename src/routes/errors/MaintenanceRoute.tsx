bde0e7ce12f0061294dbe6cea79f0ce37fa776e0
sulenq <fatwalinovera@gmail.com>
2025-08-29 20:43:32 +0700

    styling

--- src/routes/errors/MaintenanceRoute.tsx
+++ src/routes/errors/MaintenanceRoute.tsx
@@ -2,10 +2,11 @@
 
 import BButton from "@/components/ui-custom/Btn";
 import NavLink from "@/components/ui-custom/NavLink";
+import P from "@/components/ui-custom/P";
 import ExiumWatermark from "@/components/widget/ExiumWatermark";
 import useLang from "@/context/useLang";
 import { useThemeConfig } from "@/context/useThemeConfig";
-import { Text, VStack } from "@chakra-ui/react";
+import { VStack } from "@chakra-ui/react";
 
 export default function MaintenanceRoute() {
   // Contexts
@@ -15,13 +16,17 @@ export default function MaintenanceRoute() {
   return (
     <VStack h={"100vh"} gap={0}>
       <VStack p={8} flex={1} justify={"center"} gap={4} w={"full"}>
-        <Text textAlign={"center"} fontSize={32} fontWeight={600}>
-          503 Maintenance
-        </Text>
+        <P fontWeight={"bold"} fontSize={"xl"} color={"fg.subtle"}>
+          503
+        </P>
 
-        <Text textAlign={"center"} mb={4} maxW={"600px"} color={"fg.muted"}>
+        <P textAlign={"center"} fontSize={"xl"} fontWeight={"bold"}>
+          Maintenance
+        </P>
+
+        <P textAlign={"center"} mb={4} maxW={"600px"} color={"fg.muted"}>
           {l.maintenance_page.description}
-        </Text>
+        </P>
 
         <NavLink to={"/"} w={"fit"}>
           <BButton colorPalette={themeConfig.colorPalette}>
--- src/routes/errors/NotFoundRoute.tsx
+++ src/routes/errors/NotFoundRoute.tsx
@@ -3,6 +3,7 @@ export const dynamic = "force-dynamic";
 
 import BButton from "@/components/ui-custom/Btn";
 import NavLink from "@/components/ui-custom/NavLink";
+import P from "@/components/ui-custom/P";
 import ExiumWatermark from "@/components/widget/ExiumWatermark";
 import useLang from "@/context/useLang";
 import { useThemeConfig } from "@/context/useThemeConfig";
@@ -16,11 +17,15 @@ export default function NotFoundRoute() {
   return (
     <VStack h={"100vh"} gap={0}>
       <VStack p={8} flex={1} justify={"center"} gap={4} w={"full"}>
-        <Text textAlign={"center"} fontSize={32} fontWeight={600}>
-          404 Page Not Found
+        <P fontWeight={"bold"} fontSize={"xl"} color={"fg.subtle"}>
+          404
+        </P>
+
+        <Text textAlign={"center"} fontSize={"xl"} fontWeight={"bold"}>
+          Page Not Found
         </Text>
 
-        <Text textAlign={"center"} mb={4} maxW={"600px"} color={"fg.muted"}>
+        <Text textAlign={"center"} mb={4} maxW={"450px"} color={"fg.subtle"}>
           {l.missing_page.description}
         </Text>
 
