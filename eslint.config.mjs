65d080f4a740c7e4096b9b062e67fcefc391bd71
sulenq <fatwalinovera@gmail.com>
2025-08-29 17:00:07 +0700

    refactor

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -20,6 +20,7 @@ const eslintConfig = [
       "next-env.d.ts",
     ],
     rules: {
+      "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-empty-object-type": "off",
     },
   },
