444a1aaa3cb72980b2555b94244f24d4cf970745
sulenq <fatwalinovera@gmail.com>
2025-08-31 16:42:58 +0700

    styling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,17 @@
-"use client";
-
 import { Provider } from "@/components/ui/provider";
 import { Toaster } from "@/components/ui/toaster";
 import "./globals.css";
 import ClientOnly from "@/components/widget/ClientOnly";
+import { Metadata } from "next";
 
 interface Props {
   children: React.ReactNode;
 }
+
+export const metadata: Metadata = {
+  title: "Next Chakra PWA Template | Exium.id",
+  description: "Template that using Next.js, Chakra UI, and PWA by Exium.id",
+};
 
 export default function RootLayout(props: Props) {
   // Props
@@ -15,13 +19,6 @@ export default function RootLayout(props: Props) {
 
   return (
     <html suppressHydrationWarning>
-      <head>
-        <title>Next Chakra PWA Template | Exium.id</title>
-        <meta
-          name="description"
-          content="Template that using Next.js, Chakra UI, and PWA by Exium.id"
-        />
-      </head>
       <body>
         <Provider>
           <Toaster />
