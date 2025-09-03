de7eff35ccdf8528d382b526b11d36beaf04cbf6
sulenq <fatwalinovera@gmail.com>
2025-09-03 13:31:58 +0700

    refactor

--- src/routes/docs/DocsIndexRoute.tsx
+++ src/routes/docs/DocsIndexRoute.tsx
@@ -1,7 +1,16 @@
 import { CContainer } from "@/components/ui/c-container";
+import { P } from "@/components/ui/p";
 
 const DocsIndexRoute = () => {
-  return <CContainer></CContainer>;
+  return (
+    <CContainer p={4} gap={8}>
+      <P fontSize={"xl"} fontWeight={"bold"}>
+        Docs
+      </P>
+
+      <CContainer></CContainer>
+    </CContainer>
+  );
 };
 
 export default DocsIndexRoute;
