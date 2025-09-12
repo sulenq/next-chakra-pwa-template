9ce3ebe3212cc83ef802a1133a5e944b84411267
sulenq <fatwalinovera@gmail.com>
2025-09-12 09:20:59 +0700

    styling

--- src/app/pvt/settings/page.tsx
+++ src/app/pvt/settings/page.tsx
@@ -16,7 +16,7 @@ const SettingsRoute = () => {
       flex={1}
       align={"center"}
       justify={"center"}
-      // bg={"body"}
+      bg={"body"}
       p={4}
       mb={4}
       rounded={themeConfig.radii.container}
