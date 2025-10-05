ab8c9680e434183419b5d224620ab5e2dda034ba
sulenq <fatwalinovera@gmail.com>
2025-10-05 11:26:43 +0700

    styling

---
+++ src/components/widget/ClampText.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { P } from "@/components/ui/p";
+import SimplePopover from "@/components/widget/SimplePopover";
+import { TextProps } from "@chakra-ui/react";
+
+export const ClampText = (props: TextProps) => {
+  // Props
+  const { children, ...restProps } = props;
+
+  return (
+    <SimplePopover content={<P>{children}</P>}>
+      <P lineClamp={1} maxW={"300px"} {...restProps}>
+        {children}
+      </P>
+    </SimplePopover>
+  );
+};
