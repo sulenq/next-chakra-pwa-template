68ba331f5bae49071c978384dde312cb3407c29c
sulenq <fatwalinovera@gmail.com>
2025-11-14 15:33:07 +0700

    refactor

--- src/app/(private)/layout.tsx
+++ src/app/(private)/layout.tsx
@@ -1117,7 +1117,6 @@ const AppLayout = (props: Props__Layout) => {
     // Redirect only when check is done and no verified token
     if (checked && !verifiedAuthToken) {
       router.replace("/");
-      console.debug("router.replace ran");
     }
   }, [checked, verifiedAuthToken]);
 
