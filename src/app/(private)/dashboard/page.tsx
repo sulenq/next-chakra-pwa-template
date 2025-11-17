242cc00b6961ab00112748596c64ace0e73b8f88
sulenq <fatwalinovera@gmail.com>
2025-11-17 15:39:12 +0700

    refactor: wip pdf viewer

--- src/app/(private)/dashboard/page.tsx
+++ src/app/(private)/dashboard/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { PDFViewer } from "@/components/widget/PDFViewer";
 import { ReactPDFViewer } from "@/components/widget/ReactPDFViewer";
 import { TOP_BAR_H } from "@/constants/sizes";
 import { SimpleGrid } from "@chakra-ui/react";
@@ -8,6 +9,10 @@ const AdminDashboardRoute = () => {
   return (
     <SimpleGrid id="dashboard" columns={2} gap={4}>
       <ReactPDFViewer
+        fileUrl={`/assets/dummy-pdf.pdf`}
+        maxH={`calc(100vh -  ${TOP_BAR_H})`}
+      />
+      <PDFViewer
         fileUrl={`/assets/dummy-pdf.pdf`}
         maxH={`calc(100vh -  ${TOP_BAR_H})`}
       />
