025df7886ea701511f94b673c0440d40c0c7963f
sulenq <fatwalinovera@gmail.com>
2025-11-25 22:48:30 +0700

    refactor

--- src/app/(private)/dashboard/page.tsx
+++ src/app/(private)/dashboard/page.tsx
@@ -6,7 +6,7 @@ import { SimpleGrid } from "@chakra-ui/react";
 
 const AdminDashboardRoute = () => {
   return (
-    <SimpleGrid id="dashboard" columns={2} gap={4} px={4} pb={4}>
+    <SimpleGrid id="dashboard" columns={[1, null, 2]} gap={4} px={4} pb={4}>
       <PDFViewer
         fileUrl={`/assets/dummy-pdf.pdf`}
         h={`calc(100vh - ${TOP_BAR_H} - 16px)`}
