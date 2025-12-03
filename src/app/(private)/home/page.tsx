6d3ba4aae5dccfec4b7fc13adf43d5fd03a0c3a9
sulenq <fatwalinovera@gmail.com>
2025-12-03 09:36:30 +0700

    styling

--- src/app/(private)/home/page.tsx
+++ src/app/(private)/home/page.tsx
@@ -1,20 +1,14 @@
 "use client";
 
 import { CContainer } from "@/components/ui/c-container";
-import {
-  PageContainer,
-  PageContent,
-  PageTitle,
-} from "@/components/widget/Page";
+import { PageContainer, PageContent } from "@/components/widget/Page";
 import { SimpleGrid } from "@chakra-ui/react";
 
 export default function Page() {
   return (
     <PageContainer>
       <SimpleGrid flex={1} columns={[1, null, 2]}>
-        <CContainer>
-          <PageTitle justify={"space-between"} pr={3} />
-
+        <CContainer borderRight={"1px solid"} borderColor={"border.muted"}>
           <PageContent gap={1}></PageContent>
         </CContainer>
       </SimpleGrid>
