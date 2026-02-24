ff964f4fcbfa050ed6968fc68561aba700e040d7
Fatwa Linovera <fatwalinovera@gmail.com>
2026-02-24 12:56:31 +0700

    refactor: remove useMonthNames hook and its dependencies

--- src/hooks/useMonthNames.ts
+++
@@ -1,7 +0,0 @@
-import { monthNamesFromLang } from "@/constants/months";
-import useLang from "@/context/useLang";
-
-export const useMonthNames = () => {
-  const l = useLang((s) => s.l);
-  return monthNamesFromLang(l);
-};
