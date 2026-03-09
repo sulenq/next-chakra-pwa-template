9f28f13a77dcf6121a03614fef30e5be050428c4
Fatwa Linovera <fatwalinovera@gmail.com>
2026-03-09 16:12:25 +0700

    styling

--- src/components/widgets/m-container.tsx
+++ src/components/widgets/m-container.tsx
@@ -3,24 +3,37 @@ import { CContainer } from "@/components/ui/c-container";
 import { StackProps } from "@chakra-ui/react";
 
 export interface MContainerProps extends StackProps {
-  top?: string | number;
-  bottom?: string | number;
+  maskingTop?: string | number;
+  maskingBottom?: string | number;
+  maskingLeft?: string | number;
+  maskingRight?: string | number;
 }
+
 export const MContainer = forwardRef<HTMLDivElement, MContainerProps>(
   function MContainer(props, ref) {
     const {
-      top = "8px",
-      bottom = "8px",
+      maskingTop = "8px",
+      maskingBottom = "8px",
+      maskingLeft = "8px",
+      maskingRight = "8px",
       style,
       children,
       ...restProps
     } = props;
 
-    const mask = `linear-gradient(
+    const verticalMask = `linear-gradient(
       to bottom,
       transparent 0,
-      black ${top},
-      black calc(100% - ${bottom}),
+      black ${maskingTop},
+      black calc(100% - ${maskingBottom}),
+      transparent 100%
+    )`;
+
+    const horizontalMask = `linear-gradient(
+      to right,
+      transparent 0,
+      black ${maskingLeft},
+      black calc(100% - ${maskingRight}),
       transparent 100%
     )`;
 
@@ -28,8 +41,10 @@ export const MContainer = forwardRef<HTMLDivElement, MContainerProps>(
       <CContainer
         ref={ref}
         style={{
-          WebkitMaskImage: mask,
-          maskImage: mask,
+          WebkitMaskImage: `${verticalMask}, ${horizontalMask}`,
+          maskImage: `${verticalMask}, ${horizontalMask}`,
+          WebkitMaskComposite: "source-in",
+          maskComposite: "intersect",
           ...style,
         }}
         {...restProps}
