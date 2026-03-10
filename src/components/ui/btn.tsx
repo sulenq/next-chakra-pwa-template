1539a346a9c504b89c396ea6d415e4bbc2e41cae
Fatwa Linovera <fatwalinovera@gmail.com>
2026-03-10 19:16:03 +0700

    styling

--- src/components/ui/btn.tsx
+++ src/components/ui/btn.tsx
@@ -1,25 +1,25 @@
 "use client";
 
-import { useColorMode, useColorModeValue } from "@/components/ui/color-mode";
 import { MAIN_BUTTON_SIZE } from "@/constants/styles";
 import { useThemeConfig } from "@/contexts/useThemeConfig";
 import { Button, ButtonProps, IconButton } from "@chakra-ui/react";
-import { forwardRef, useMemo } from "react";
+import { forwardRef } from "react";
 
-export interface BtnProps extends ButtonProps {
+export interface Props__Btn extends ButtonProps {
   children?: React.ReactNode;
   clicky?: boolean;
   iconButton?: boolean;
   focusStyle?: boolean;
 }
 
-export const Btn = forwardRef<HTMLButtonElement, BtnProps>((props, ref) => {
+export const Btn = forwardRef<HTMLButtonElement, Props__Btn>((props, ref) => {
   // Props
   const {
     children,
+    className = "",
     clicky = true,
     iconButton = false,
-    className = "",
+    colorPalette = "gray",
     size,
     focusStyle = true,
     ...restProps
@@ -27,55 +27,18 @@ export const Btn = forwardRef<HTMLButtonElement, BtnProps>((props, ref) => {
 
   // Contexts
   const { themeConfig } = useThemeConfig();
-  const { colorMode } = useColorMode();
-  const graySolidActiveBg = useColorModeValue("gray.600", "gray.300");
 
-  // States
+  // Derived Values
   const resolvedClassName = `${clicky ? "clicky" : ""} ${className}`.trim();
-  const isSubtle = props?.variant === "subtle";
-  const isColorPaletteGray = props?.colorPalette === "gray";
-
-  // Memoized Active Style
-  const resolvedMutedColor = useColorModeValue(
-    `${props.colorPalette}.200 !important`,
-    `${props.colorPalette}.800 !important`,
-  );
-  const activeBg = useMemo(() => {
-    if (props.colorPalette) {
-      switch (props?.variant) {
-        default:
-          return isColorPaletteGray
-            ? graySolidActiveBg
-            : `${props.colorPalette}.600`;
-        case "ghost":
-        case "outline":
-          return `${props.colorPalette}.subtle`;
-        case "subtle":
-        case "surface":
-          return resolvedMutedColor;
-        case "plain":
-          return "";
-      }
-    } else {
-      switch (props?.variant) {
-        default:
-          return "";
-        case "subtle":
-        case "surface":
-          return "gray.muted";
-      }
-    }
-  }, [props.variant, props.colorPalette, colorMode]);
 
   return iconButton ? (
     <IconButton
       ref={ref}
-      fontSize={"md"}
       className={resolvedClassName}
+      colorPalette={colorPalette}
       size={size || (MAIN_BUTTON_SIZE as any)}
       rounded={themeConfig.radii.component}
-      _hover={{ bg: activeBg }}
-      _active={{ bg: activeBg }}
+      fontSize={"md"}
       _focusVisible={
         focusStyle
           ? {
@@ -92,13 +55,11 @@ export const Btn = forwardRef<HTMLButtonElement, BtnProps>((props, ref) => {
     <Button
       ref={ref}
       className={resolvedClassName}
+      colorPalette={colorPalette}
+      size={size || (MAIN_BUTTON_SIZE as any)}
       fontSize={"md"}
       fontWeight="medium"
-      // bg={isSubtle && !props.colorPalette ? "d0" : ""}
-      size={size || (MAIN_BUTTON_SIZE as any)}
       rounded={themeConfig.radii.component}
-      _hover={{ bg: isSubtle && !props.colorPalette ? "d1" : activeBg }}
-      _active={{ bg: activeBg }}
       _focusVisible={
         focusStyle
           ? {
@@ -114,4 +75,12 @@ export const Btn = forwardRef<HTMLButtonElement, BtnProps>((props, ref) => {
   );
 });
 
+export const PBtn = forwardRef<HTMLButtonElement, Props__Btn>((props, ref) => {
+  // Contexts
+  const { themeConfig } = useThemeConfig();
+
+  return <Btn ref={ref} colorPalette={themeConfig.colorPalette} {...props} />;
+});
+
 Btn.displayName = "Btn";
+PBtn.displayName = "PBtn";
