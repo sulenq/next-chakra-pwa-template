3b85ed39e84fc8445938d150202889bbbea0a1bb
Fatwa Linovera <fatwalinovera@gmail.com>
2026-03-25 08:49:03 +0700

    styling

--- src/theme/chakraCustomSystem.ts
+++ src/theme/chakraCustomSystem.ts
@@ -544,7 +544,7 @@ const customConfig = defineConfig({
             value: {
               base: "rgba(255, 255, 255, 0.5)",
               // _dark: "rgba(25, 25, 25, 0.5)",
-              _dark: "rgba(25, 25, 25, 0.5)",
+              _dark: "rgba(24, 24, 24, 0.75)",
             },
           },
           subtle: {
