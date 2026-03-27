340d1104b3b8f0056ef9ed114811b715b7dae74f
Fatwa Linovera <fatwalinovera@gmail.com>
2026-03-27 08:12:28 +0700

    styling

--- src/app/(private)/welcome/page.tsx
+++ src/app/(private)/welcome/page.tsx
@@ -1,14 +1,12 @@
 "use client";
 
-import { useColorModeValue } from "@/components/ui/color-mode";
 import { P } from "@/components/ui/p";
 import { StackV } from "@/components/ui/stack";
 import BrandWatermark from "@/components/widgets/brand-watermark";
-import { Logo } from "@/components/widgets/logo";
 import { View } from "@/components/widgets/view";
 import { APP } from "@/constants/_meta";
 import { useLocale } from "@/contexts/useLocale";
-import { pluckString } from "@/utils/string";
+import { interpolateString, pluckString } from "@/utils/string";
 import { VStack } from "@chakra-ui/react";
 
 export default function Page() {
@@ -18,21 +16,23 @@ export default function Page() {
   // States
   const variantNumber = Math.floor(Math.random() * 16) + 1;
 
-  // Constants
-  const logoColor = useColorModeValue("var(--chakra-colors-d1)", "#0f0f0f");
-
   return (
     <View.Root p={4}>
       <VStack flex={1} gap={1} justify={"center"}>
         <StackV align={"center"} my={"auto"}>
-          <Logo size={65} color={logoColor} mb={4} />
-
           <StackV align={"center"} color={"fg.subtle"}>
-            <P fontSize={"xl"} fontWeight={"medium"} textAlign={"center"}>
-              {APP.name}
+            <P fontSize={"lg"} fontWeight={"medium"} textAlign={"center"}>
+              {interpolateString(pluckString(t, `msg_welcome_to_the_app`), {
+                appName: APP.name,
+              })}
             </P>
 
-            <P textAlign={"center"}>
+            <P
+              fontSize={"xl"}
+              fontWeight={"medium"}
+              color={"ibody"}
+              textAlign={"center"}
+            >
               {pluckString(t, `msg_welcome_${variantNumber}`)}
             </P>
           </StackV>
