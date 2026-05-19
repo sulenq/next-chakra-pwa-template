dbe2c9d19e4a38a12c0d0dcead002240c69b4883
Fatwa Linovera <fatwalinovera@gmail.com>
2026-05-19 15:51:09 +0700

    refactor: slicing

---
+++ src/features/settings/activity-log/sections/activity-log.section.tsx
@@ -0,0 +1,163 @@
+"use client";
+
+import { Item } from "@/components/container/item";
+import { DataListFooter } from "@/components/data-list/data-footer";
+import FeedbackNoData from "@/components/feedback/feedback-no-data";
+import FeedbackNotFound from "@/components/feedback/feedback-not-found";
+import FeedbackRetry from "@/components/feedback/feedback-retry";
+import { SettingsHelperText } from "@/components/ui/helper-text";
+import { P } from "@/components/ui/p";
+import { SearchInput } from "@/components/ui/search-input";
+import { Skeleton } from "@/components/ui/skeleton";
+import { StackH, StackV } from "@/components/ui/stack";
+import { MiniUser } from "@/components/user/mini-user";
+import { dummyAllActivityLogs } from "@/constants/dummy-data";
+import { ActivityActionEnum } from "@/constants/enums";
+import { R_SPACING_MD } from "@/constants/styles";
+import { useLocaleContext } from "@/features/settings/regional/contexts/use-locale-context";
+import { useFetchData } from "@/hooks/useFetchData";
+import type { ActivityLog } from "@/types/global.types";
+import { isEmptyArray } from "@/utils/array";
+import { formatDate } from "@/utils/formatter";
+import { useState } from "react";
+
+// -----------------------------------------------------------------
+
+export const ActivityLogSection = () => {
+  // Contexts
+  const { t } = useLocaleContext();
+
+  // States
+  const [search, setSearch] = useState("");
+  const {
+    error,
+    initialLoading,
+    data,
+    onRetry,
+    limit,
+    setLimit,
+    pagination,
+    page,
+    setPage,
+  } = useFetchData<{
+    totalData: number;
+    items: ActivityLog[];
+  }>({
+    initialData: {
+      totalData: 100,
+      items: dummyAllActivityLogs,
+    },
+    url: ``,
+    dataResource: false,
+  });
+
+  // Derived Values
+  const activityFormatter: Record<
+    string,
+    (meta?: Record<string, any>) => string
+  > = {
+    // TODO_DEV create action sentence glosary
+    [ActivityActionEnum.CREATE_WORKSPACE]: (meta) =>
+      `Created workspace "${meta?.workspaceName ?? "Unknown"}"`,
+
+    [ActivityActionEnum.UPDATE_WORKSPACE]: (meta) =>
+      `Updated workspace "${meta?.workspaceName ?? "Unknown"}"`,
+
+    [ActivityActionEnum.DELETE_WORKSPACE]: (meta) =>
+      `Deleted workspace "${meta?.workspaceName ?? "Unknown"}"`,
+
+    [ActivityActionEnum.CREATE_LAYER]: (meta) =>
+      `Created layer "${meta?.layerName ?? "Unknown"}"`,
+
+    [ActivityActionEnum.UPDATE_LAYER]: (meta) =>
+      `Updated layer "${meta?.layerName ?? "Unknown"}"`,
+
+    [ActivityActionEnum.DELETE_LAYER]: (meta) =>
+      `Deleted layer "${meta?.layerName ?? "Unknown"}`,
+  };
+  const formatActivityLog = (log: ActivityLog): string => {
+    return activityFormatter[log.action as ActivityActionEnum](log.metadata);
+  };
+
+  // Render State Map
+  const render = {
+    loading: <Skeleton flex={1} />,
+    error: <FeedbackRetry onRetry={onRetry} />,
+    empty: <FeedbackNoData />,
+    notFound: <FeedbackNotFound />,
+    loaded: (
+      <>
+        {data?.items?.map((log, index) => {
+          return (
+            <StackH
+              align={"center"}
+              key={`${log.id}-${index}`}
+              gap={4}
+              p={4}
+              justify={"space-between"}
+              borderTop={index === 0 ? "" : "1px solid"}
+              borderColor={"border.subtle"}
+            >
+              {log.user && <MiniUser withEmail user={log.user} w={"240px"} />}
+
+              <StackV flex={1}>
+                <P>{formatActivityLog(log)}</P>
+
+                <P color={"fg.subtle"}>
+                  {formatDate(log?.createdAt, t, {
+                    variant: "dayShortMonthYear",
+
+                    withTime: true,
+                  })}
+                </P>
+              </StackV>
+            </StackH>
+          );
+        })}
+      </>
+    ),
+  };
+
+  return (
+    <Item.Root px={R_SPACING_MD}>
+      <SettingsHelperText>{t.activity_logs}</SettingsHelperText>
+
+      <Item.Body>
+        <StackV p={4}>
+          <SearchInput
+            onChange={(inputValue) => {
+              setSearch(inputValue || "");
+            }}
+            inputValue={search}
+            queryKey={"q-activity-auth"}
+          />
+        </StackV>
+
+        <StackV minH={"300px"}>
+          {initialLoading && render.loading}
+          {!initialLoading && (
+            <>
+              {error && render.error}
+              {!error && (
+                <>
+                  {data?.items && render.loaded}
+                  {(!data?.items || isEmptyArray(data?.items)) && render.empty}
+                </>
+              )}
+            </>
+          )}
+        </StackV>
+
+        <DataListFooter
+          limit={limit}
+          setLimit={setLimit}
+          dataLength={data?.items?.length}
+          totalData={data?.totalData}
+          page={page}
+          setPage={setPage}
+          totalPage={pagination?.meta?.totalPage}
+        />
+      </Item.Body>
+    </Item.Root>
+  );
+};
---
+++ src/features/settings/integration/sections/api-key.section.tsx
@@ -0,0 +1,106 @@
+"use client";
+
+import { Item } from "@/components/container/item";
+import { Btn } from "@/components/ui/btn";
+import { Field, FieldsetRoot } from "@/components/ui/field";
+import { SettingsHelperText } from "@/components/ui/helper-text";
+import { PasswordInput } from "@/components/ui/password-input";
+import { StackH } from "@/components/ui/stack";
+import { R_SPACING_MD } from "@/constants/styles";
+import useRenderTrigger from "@/contexts/use-render-trigger";
+import { useThemeContext } from "@/features/settings/display/contexts/use-theme-context";
+import { useLocaleContext } from "@/features/settings/regional/contexts/use-locale-context";
+import { useRequest } from "@/hooks/useRequestOld";
+import { useFormik } from "formik";
+import * as yup from "yup";
+
+export const APIKeySection = () => {
+  // Contexts
+  const { t } = useLocaleContext();
+  const { themeContext } = useThemeContext();
+  const setRt = useRenderTrigger((s) => s.setRt);
+
+  // Hooks
+  const { req, loading } = useRequest({
+    id: "",
+  });
+
+  // States
+  const formik = useFormik({
+    validateOnChange: false,
+    initialValues: {
+      mapboxToken: "",
+      tinyMceToken: "",
+    },
+    validationSchema: yup.object().shape({
+      mapboxToken: yup.string().required(t.msg_required_form),
+      tinyMceToken: yup.string().required(t.msg_required_form),
+    }),
+    onSubmit: (values) => {
+      // console.debug(values);
+
+      const config = {
+        url: ``,
+        method: "PATCH",
+        data: values,
+      };
+
+      req({
+        config,
+        onResolve: {
+          onSuccess: () => {
+            setRt((ps) => !ps);
+          },
+        },
+      });
+    },
+  });
+
+  return (
+    <Item.Root px={R_SPACING_MD}>
+      <SettingsHelperText>API Keys</SettingsHelperText>
+
+      <Item.Body p={4}>
+        <form id={"api-keys-form"} onSubmit={formik.handleSubmit}>
+          <FieldsetRoot disabled={loading}>
+            <Field
+              label={"Mapbox Token"}
+              invalid={!!formik.errors.mapboxToken}
+              errorText={formik.errors.mapboxToken as string}
+            >
+              <PasswordInput
+                inputValue={formik.values.mapboxToken}
+                onChange={(inputValue) => {
+                  formik.setFieldValue("mapboxToken", inputValue);
+                }}
+              />
+            </Field>
+
+            <Field
+              label={"Tiny MCE Token"}
+              invalid={!!formik.errors.tinyMceToken}
+              errorText={formik.errors.tinyMceToken as string}
+            >
+              <PasswordInput
+                inputValue={formik.values.tinyMceToken}
+                onChange={(inputValue) => {
+                  formik.setFieldValue("tinyMceToken", inputValue);
+                }}
+              />
+            </Field>
+          </FieldsetRoot>
+        </form>
+
+        <StackH align={"center"} justify={"end"} mt={8}>
+          <Btn
+            type={"submit"}
+            form={"api-keys-form"}
+            colorPalette={themeContext.colorPalette}
+          >
+            {t.save}
+          </Btn>
+        </StackH>
+      </Item.Body>
+    </Item.Root>
+  );
+};
